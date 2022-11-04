aa7140819bcf2118411e4ea625798b592a7d291d
olganazarenko <113229061+olganazarenko@users.noreply.github.com>
2022-11-04 21:25:57 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -165,8 +165,7 @@ class AddressBook:
             if name == contact.name.value:
                 return contact
 
-    # This additional and temporary command was created because all data here is saved in the file, and it is impossible 
-    # to change object in this format.
+    # Temporary command due to impossibility to change object in the file
     def change_contact(self, contact: Record, remove: bool = False) -> None:
         temporary_filename = 'old_contacts.json'
 
