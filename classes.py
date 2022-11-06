8a05c8416d1d1eb850335f133212485d6e886d59
olganazarenko <113229061+olganazarenko@users.noreply.github.com>
2022-11-06 18:38:05 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,12 +1,7 @@
-import json
-from json import JSONEncoder
-from json import JSONDecoder
 import re
 from collections import UserDict
 import constants
 from datetime import date
-from typing import Generator
-import os
 
 
 class Field:
@@ -23,6 +18,7 @@ class Field:
 
     def __repr__(self):
         return f"{self.__class__.__name__}(value={self.value})"
+#represents the name of the class
 
 
 class Name(Field):
@@ -49,12 +45,12 @@ class Phone(Field):
 
 
 class Birthday(Field):
-    def __init__(self, value: str):
+    def __init__(self, value):
         super().__init__(value)
-        self.value: date = value
+        self.value = value
 
     @Field.value.setter
-    def value(self, value: str):
+    def value(self, value):
         self._value = self._check_birthday(value)
 
     @staticmethod
@@ -111,27 +107,15 @@ class Record:
         return "Record({})".format(', '.join([f"{k}={v!r}" for k, v in self.__dict__.items()]))
 
 
-class AddressBook:
-
-    def __init__(self):
-        self.file_name = "common_contacts.json"
-        open(self.file_name, 'a').close()
+class AddressBook(UserDict):
 
     def add_record(self, record: Record):
-        contact = {
-            'name': record.name.value,
-            'phones': [x.value for x in record.phones],
-            'birthday': record.birthday.value.strftime('%d.%m.%Y') if record.birthday else None
-        }
+        self.data[record.name.value] = record
 
-        with open(self.file_name, 'a') as file:
-            contact = json.dumps(contact)
-            file.write(contact + '\n')
-
-    def iterator(self, count: int) -> 'Generator[list[Record, ...]]':
+    def iterator(self, count: int):
         contacts_iteration = []
 
-        for contact in self.__read_file():
+        for contact in self.data.values():
             contacts_iteration.append(contact)
 
             if len(contacts_iteration) >= count:
@@ -140,64 +124,3 @@ class AddressBook:
 
         if contacts_iteration:
             yield contacts_iteration
-
-    def __read_file(self) -> 'Generator[Record]':
-        with open(self.file_name, 'r') as file:
-            for line in file:
-                contact = json.loads(line)
-                record = Record(contact['name'])
-                record.birthday = Birthday(contact['birthday']) if contact['birthday'] else ''
-                record.phones = [Phone(x) for x in contact['phones']]
-                yield record
-
-    def search_contacts(self, value: str) -> list[Record, ...] | None:
-        found_contacts = []
-
-        for contact in self.__read_file():
-            if value in contact.name.value or any(value in str(phone.value) for phone in contact.phones):
-                found_contacts.append(contact)
-
-        if found_contacts:
-            return found_contacts
-
-    def get_contact(self, name: str) -> Record | None:
-        for contact in self.__read_file():
-            if name == contact.name.value:
-                return contact
-
-    # Temporary command due to impossibility to change object in the file
-    def change_contact(self, contact: Record, remove: bool = False) -> None:
-        temporary_filename = 'old_contacts.json'
-
-        os.rename(self.file_name, temporary_filename)
-
-        self.file_name = temporary_filename
-
-        for _contact in self.__read_file():
-            if _contact.name.value == contact.name.value:
-                if remove:
-                    continue
-
-                AddressBook().add_record(contact)
-
-            else:
-                AddressBook().add_record(_contact)
-
-        os.remove(temporary_filename)
-        self.file_name = AddressBook().file_name
-
-    def __getitem__(self, item):
-        for contact in self.__read_file():
-            if item == contact.name.value:
-                return contact
-        else:
-            raise KeyError(item)
-
-
-if __name__ == '__main__':
-    c = AddressBook()
-    c.add_record(Record('Apple', '', '22.10.2000'))
-    c.add_record(Record('Sugar', '0931111212', ''))
-    c.add_record(Record('Salt', '0931111212', '22.10.2000'))
-    for x in c.iterator(1):
-        print(x)
